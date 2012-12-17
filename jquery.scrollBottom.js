7975e3529ca0a6e4eda3e3e62ce617c8c4661551
João Gonçalves <goncalves.joao@gmail.com>
2012-12-17 14:47:42 +0000

    turned the scrollBottom function into a jQuery plugin

--- jquery.scrollBottom.js
+++ jquery.scrollBottom.js
@@ -1,7 +1,20 @@
 /*!
- * jquery.scrollBottom.js v0.0.1 - 17 December, 2012
+ * jquery.scrollBottom.js v0.1.0 - 17 December, 2012
  * By João Gonçalves (http://goncalvesjoao.github.com)
  * Hosted on https://github.com/goncalvesjoao/jQuery-scrollBottom
  * Licensed under MIT license.
  */
 
+(function($){
+  $.fn.scrollBottom = function(callback) {
+    return this.each(function() {
+      $(this).scroll(function() {
+        var container = $(this);
+        if ((container[0].scrollHeight - container.scrollTop()) < (container.outerHeight())) {
+          container.trigger('scrollBottom');
+        }
+      });
+      $(this).bind('scrollBottom', callback);
+    });
+  };
+})(jQuery);
