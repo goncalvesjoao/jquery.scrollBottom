9a5f7761b0162945e89de7f53ab70ec7f5966b59
João Gonçalves <goncalves.joao@gmail.com>
2012-12-17 13:41:39 +0000

    first commit

---
+++ jquery.scrollBottom.js
@@ -0,0 +1,7 @@
+/*!
+ * jquery.scrollBottom.js v0.0.1 - 17 December, 2012
+ * By João Gonçalves (http://goncalvesjoao.github.com)
+ * Hosted on https://github.com/goncalvesjoao/jQuery-scrollBottom
+ * Licensed under MIT license.
+ */
+
