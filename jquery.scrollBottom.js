767f2d3f1c0f0e40a73c9eefeced8cd5131571b6
João Gonçalves <goncalves.joao@gmail.com>
2012-12-17 15:01:49 +0000

    scrollBottom accepts a margin_bottom param

--- jquery.scrollBottom.js
+++ jquery.scrollBottom.js
@@ -1,16 +1,17 @@
 /*!
- * jquery.scrollBottom.js v0.1.0 - 17 December, 2012
+ * jquery.scrollBottom.js v0.1.2 - 17 December, 2012
  * By João Gonçalves (http://goncalvesjoao.github.com)
  * Hosted on https://github.com/goncalvesjoao/jQuery-scrollBottom
  * Licensed under MIT license.
  */
 
 (function($){
-  $.fn.scrollBottom = function(callback) {
+  $.fn.scrollBottom = function(callback, margin_bottom) {
+    if (margin_bottom == undefined) margin_bottom = 0;
     return this.each(function() {
       $(this).scroll(function() {
         var container = $(this);
-        if ((container[0].scrollHeight - container.scrollTop()) < (container.outerHeight())) {
+        if ((container[0].scrollHeight - container.scrollTop()) < (container.outerHeight() + margin_bottom)) {
           container.trigger('scrollBottom');
         }
       });
